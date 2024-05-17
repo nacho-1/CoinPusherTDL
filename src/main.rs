4328f139c0f757756dc7fb61cff59c4723594966
ibertolini <56204692+nacho-1@users.noreply.github.com>
2024-05-17 15:43:17 -0300

    feat: Implmento input de usuario
    Funcion para leer el input de usuario.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::io;
+use std::process;
+use std::error::Error;
+
+const INSERT_KEY: char = 't';
+const ASK_KEY: char = 'y';
+const QUIT_KEY: char = 'q';
+
 fn main() {
-    println!("Hello, world!");
+    let option = read_option().unwrap_or_else(|err| {
+        eprintln!("Error leyendo la opción: {err}");
+        process::exit(1);
+    });
+
+    println!("La opción elegida es: {option}");
+}
+
+fn read_option() -> Result<char, Box<dyn Error>> {
+    loop {
+        println!("Ingresá una accion:");
+        println!(" {INSERT_KEY} : Ingresar moneda");
+        println!(" {ASK_KEY} : Consultar cantidad");
+        println!(" {QUIT_KEY} : Salir");
+
+        let mut input = String::new();
+
+        io::stdin().read_line(&mut input)?;
+
+        let trimmed_input = input.trim();
+
+        if trimmed_input.len() != 1 {
+            println!("Opción invalida. Intente nuevamente\n");
+            continue;
+        }
+
+        // Should never panic
+        let option = trimmed_input.chars().next().unwrap();
+        return Ok(option);
+    }
 }
