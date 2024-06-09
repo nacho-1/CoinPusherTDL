a379d0aedfd31a4ed14075927c6c317cd4b3de9e
Ignacio Fernández <ignacio.fernandez@infiniteloop.com.ar>
2024-06-09 03:22:56 -0300

    Uso bytes

---
+++ src/protocol.rs
@@ -0,0 +1,57 @@
+pub struct Message {
+    pub action: Action,
+    pub value: String,
+}
+
+pub enum Action {
+    Insert,
+    Quit,
+    ConsultPool,
+}
+
+impl Action {
+    fn to_char(&self) -> char {
+        match self {
+            Action::Insert => 't',
+            Action::Quit => 'q',
+            Action::ConsultPool => 'y',
+        }
+    }
+
+    fn from_char(c: char) -> Option<Action> {
+        match c {
+            't' => Some(Action::Insert),
+            'q' => Some(Action::Quit),
+            'y' => Some(Action::ConsultPool),
+            _ => None,
+        }
+    }
+}
+
+pub struct Protocol;
+
+impl Protocol {
+    pub fn encode(action: Action, value: String) -> Vec<u8> {
+        let action_char = action.to_char();
+        format!("{}{}", action_char, value).into_bytes()
+    }
+    
+    pub fn decode(bytes: &[u8]) -> Message {
+        let input = std::str::from_utf8(bytes).expect("Invalid input: Cannot convert bytes to UTF-8 string");
+
+        if input.is_empty() {
+            panic!("Invalid input: String is empty");
+        }
+
+        let action_char = input.chars().next().unwrap();
+        let action = Action::from_char(action_char).expect("Invalid input: The action provided is unknown");
+
+        let value = input[1..].to_string();
+
+
+        Message {
+            action,
+            value,
+        }
+    }
+}
